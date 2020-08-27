1e6fa577061629546a312b252ae1ae4cf8252e7e
Daniel Kappel <ijinboo@googlemail.com>
2020-08-27 04:46:39 +0200

    Update testbot.py

--- testbot.py
+++ testbot.py
@@ -1,34 +1,27 @@
 import discord
 
 class MyClient(discord.Client):
-    #Einloggen
+    #Login Message 
     async def on_ready(self):
-        print("Ich habe mich eingeloggt. Mööp möp")
+        print("welcome back, let's rock the discord")
 
-    #Wenn nachrichten geschrieben werden kommt es in den Log
+    #Log when user starts to writes a message
     async def on_typing(self, channel, user, when):
-        print(str(user) + " tippt gerade in " + str(channel) + " channel seit " + str(when))
+        print(str(user) + " Is typing in " + str(channel) + " channel since " + str(when))
 
-    #Gelöschte nachrichten loggen
+    #Log deleted messages
     async def on_message_delete(self, message):
-        print("Gelöschte Nachticht " + message.content)
+        print("Deleted message " + message.content)
 
-    #Editierte nachrichten loggen
+    #Log edited messages
     async def on_message_edit(self, before, after):
         print("Changed message " + before.content + " to " + after.content)
 
 
-    #Wenn Nachrichten Persöhnlich gepostet werden
+    #Bot respond to a users text or word
     async def on_message(self, message):
-       if message.author == client.user:
-           return
-       if message.content.startswith("Hi"): #ausgabe des benutzers
-           await message.channel.send("Ne oder?") #ausgabe des bot's
-           else
-       if message.content.startwith("Moin"):
-           await message.channel.send("Moin moin!")
-
-
+       if message.content.startswith("Hi"): #output what the user say's
+           await message.channel.send("Hello") #output what the bot will say
 
 client = MyClient()
-client.run("12345679")
+client.run("my bot token")
